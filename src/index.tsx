9ed3bbb7978cbfbf0351b3d70477126e300f934a
Johsua Gore (Desktop) <Joshua.Gore8@gmail.com>
2019-08-19 14:38:38 +0930

    added graphql client

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'typeface-roboto';
-/*
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap-css-only/css/bootstrap.min.css';
-import 'mdbreact/dist/css/mdb.css';
-*/
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
 
+const client = new ApolloClient({
+  uri: 'https://wty-dev-hasura.herokuapp.com/v1/graphql',
+});
+client
+  .query({
+    query: gql`
+    {
+  lines(where: {line_routes: {}}) {
+    geom
+    line_routes {
+      route {
+        id
+        short_title
+      }
+    }
+    line_types {
+      type {
+        name
+      }
+    }
+  }
+}
+
+    `
+  })
+  .then(result => console.log(result));
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
