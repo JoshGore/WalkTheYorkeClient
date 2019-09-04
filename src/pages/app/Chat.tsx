17fcb563c933b735c2fb6a1458237665fd7ab029
Johsua Gore (Desktop) <Joshua.Gore8@gmail.com>
2019-09-04 16:47:47 +0930

    remove call to CommentForm in Chat

--- src/pages/app/Chat.tsx
+++ src/pages/app/Chat.tsx
@@ -7,36 +7,38 @@ import Comments from './chat/Comments';
 import CommentForm from './chat/CommentForm';
 import TrailContext, { TrailContextProps } from '../../contexts/TrailContext';
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
-  root: {
-    backgroundColor: '#F5F5F5',
-    padding: theme.spacing(2),
-  },
-  commentFrom: {
-    borderRadius: '15px 1px 15px 15px',
-    padding: theme.spacing(1),
-    marginBottom: theme.spacing(2),
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText,
-  },
-  commentFromAvatar: {
-    backgroundColor: theme.palette.primary.main,
-  },
-  commentTo: {
-    borderRadius: '1px 15px 15px 15px',
-    padding: theme.spacing(2),
-    paddingTop: theme.spacing(0.5),
-    marginBottom: theme.spacing(2),
-  },
-  textField: {
-    '& fieldset': {
-      borderRadius: '15px 15px 15px 15px',
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      backgroundColor: '#F5F5F5',
+      padding: theme.spacing(2),
     },
-  },
-  dense: {
-    marginTop: theme.spacing(2),
-  },
-}));
+    commentFrom: {
+      borderRadius: '15px 1px 15px 15px',
+      padding: theme.spacing(1),
+      marginBottom: theme.spacing(2),
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.primary.contrastText,
+    },
+    commentFromAvatar: {
+      backgroundColor: theme.palette.primary.main,
+    },
+    commentTo: {
+      borderRadius: '1px 15px 15px 15px',
+      padding: theme.spacing(2),
+      paddingTop: theme.spacing(0.5),
+      marginBottom: theme.spacing(2),
+    },
+    textField: {
+      '& fieldset': {
+        borderRadius: '15px 15px 15px 15px',
+      },
+    },
+    dense: {
+      marginTop: theme.spacing(2),
+    },
+  }),
+);
 
 const Chat: React.FC = () => {
   const classes = useStyles();
@@ -44,9 +46,6 @@ const Chat: React.FC = () => {
     <Grid className={classes.root} container>
       <Grid item xs={12}>
         <Comments />
-      </Grid>
-      <Grid item xs={12}>
-        <CommentForm />
       </Grid>
     </Grid>
   );
